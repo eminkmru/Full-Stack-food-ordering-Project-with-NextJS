f95c5acac9885a3562b07e297e55cc0e28279b4d
eminkmru <eminkumru135@outlook.com>
2023-07-03 01:27:37 +0300

    setup to React Formik

--- components/Reservation.jsx
+++ components/Reservation.jsx
@@ -1,40 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "./ui/Title";
 import Input from "./form/Input";
-
+import { useFormik } from "formik";
+import { CircularProgress } from "@mui/material";
 const Reservation = () => {
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  const onSubmit = async (values, actions) => {
+    setIsButtonDisabled(true);
+    await new Promise((resolve) => setTimeout(resolve, 4000));
+    actions.resetForm();
+    setIsButtonDisabled(false);
+  };
+
+  const { values, handleChange, handleSubmit } = useFormik({
+    initialValues: {
+      fullName: "",
+      phoneNumber: "",
+      email: "",
+      persons: "",
+      date: "",
+    },
+    onSubmit,
+  });
   const inputs = [
     {
       id: 1,
       name: "fullName",
       type: "text",
       placeholder: "Your Full Name",
+      value: values.fullName,
     },
     {
       id: 2,
       name: "phoneNumber",
       type: "number",
       placeholder: "Your Phone Number",
+      value: values.phoneNumber,
     },
     {
       id: 3,
       name: "email",
       type: "email",
       placeholder: "Your Email Adress",
+      value: values.email,
+    },
+    {
+      id: 5,
+      name: "persons",
+      type: "number",
+      placeholder: "How Many Persons?",
+      value: values.persons,
+    },
+    {
+      id: 4,
+      name: "date",
+      type: "datetime-local",
+      placeholder: "",
+      value: values.date,
     },
   ];
   return (
     <div className="container mx-auto py-12">
+      <Title addClass="text-[2.5rem] mb-10">Book A Table</Title>
       <div className="flex justify-between items-center gap-x-10 flex-wrap">
-        <div className="md:flex-1 w-full">
-          <Title addClass="text-[2.5rem] mb-10">Book A Table</Title>
+        <form className="md:flex-1 w-full" onSubmit={handleSubmit}>
           <div className="flex flex-col gap-y-3">
             {inputs.map((input) => (
-              <Input key={input.id} {...input} />
+              <Input key={input.id} {...input} onChange={handleChange} />
             ))}
           </div>
-          <button className="btn-primary mt-4">Book Now</button>
-        </div>
+          <button className="btn-primary mt-4" type="submit">
+            {isButtonDisabled ? <CircularProgress size={15} /> : "Book Now"}
+          </button>
+        </form>
         <div className="md:flex-1 w-full">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15647.252594303785!2d34.53820105369025!3d36.77569621331589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x15278be06f562813%3A0x66440c856df61d13!2sOTTOBros%20Burger%20%26%20More%20%C3%87iflikk%C3%B6y!5e0!3m2!1str!2str!4v1688299576134!5m2!1str!2str"
