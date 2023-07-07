e546697424c295eeb2e3693fcef1949b4cf6fbf6
eminkmru <eminkumru135@outlook.com>
2023-07-07 15:49:03 +0300

    setup mongoDB

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -16,7 +16,7 @@ export default function Document() {
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
           integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
           crossorigin="anonymous"
-          referrerpolicy="no-referrer"
+          referrerPolicy="no-referrer"
         />
       </Head>
       <body>
