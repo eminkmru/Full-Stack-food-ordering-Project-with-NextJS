0cbd97cf1ae2f9a1fe044c235653844556bffada
Emin Kumru <eminkumru135@outlook.com>
2023-07-31 21:22:22 +0300

    menu page filter

--- pages/menu/index.jsx
+++ pages/menu/index.jsx
@@ -2,18 +2,25 @@ import React from "react";
 import MenuWrapper from "../../components/product/MenuWrapper";
 import axios from "axios";
 
-const Index = ({ categoryList }) => {
+const Index = ({ categoryList, productList }) => {
   return (
     <div className="pt-10">
-      <MenuWrapper categoryList={categoryList} />
+      <MenuWrapper categoryList={categoryList} productList={productList} />
     </div>
   );
 };
 
 export const getServerSideProps = async () => {
-  const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/categories`);
+  const category = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_URL}/categories`
+  );
+  const product = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_URL}/products`
+  );
   return {
-    props: { categoryList: res.data ? res.data : [] },
+    props: { categoryList: category.data ? category.data : [] },
+    props: { productList: product.data ? product.data : [] },
   };
 };
+
 export default Index;
