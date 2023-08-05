731f85cb8f47087e9e2f4350f7e6136eb9a19500
Emin Kumru <eminkumru135@outlook.com>
2023-08-05 18:39:31 +0300

    route change progress

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,17 +1,24 @@
 import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
+import "react-toastify/dist/ReactToastify.css";
 import "../styles/globals.css";
+import "slick-carousel/slick/slick-theme.css";
+import "nprogress/nprogress.css";
+
 import Layout from "../layout/Layout";
 
 import { Provider } from "react-redux";
 import store from "../redux/store";
-
 import { SessionProvider } from "next-auth/react";
 
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
+import { Router } from "next/router";
+
+import nProgress from "nprogress";
+
+Router.events.on("routeChangeStart", () => nProgress.start());
+Router.events.on("routeChangeComplete", () => nProgress.done());
+Router.events.on("routeChangeError", () => nProgress.done());
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -17,7 +17,6 @@ const Login = () => {
         values
       );
       if (res.status === 200) {
-        console.log(res.data);
         actions.resetForm();
         toast.success("Admin Login Success!");
         push("/admin/profile");
